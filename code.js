4f63be52d37ba4f0537062aee3603a1985bf6b13
yashmanijain <yash.jain@binmile.com>
2021-11-15 11:35:23 +0530

    Initial Commit

--- code.js
+++ code.js
@@ -2,11 +2,11 @@ document.getElementById("submit").addEventListener("click",bmical);
 
 function bmical()
 {
-    var cm = parseInt(document.getElementById("cm").value);
-    var kg = parseFloat(document.getElementById("kg").value);
+    const cm = parseInt(document.getElementById("cm").value);
+    const kg = parseFloat(document.getElementById("kg").value);
 
-    var bmi;
-    var newCm = parseFloat(cm/100);
+    let bmi;
+    let newCm = parseFloat(cm/100);
 
     bmi = kg / (newCm * newCm);
     bmi = bmi.toFixed(1);
